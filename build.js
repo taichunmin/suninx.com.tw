2ae44612f98dd9eb451dbf24399f6498b54d68b4
taichunmin <taichunmin@gmail.com>
2021-03-16 02:05:46 +0800

    20210316

---
+++ build.js
@@ -0,0 +1,60 @@
+require('dotenv').config()
+
+const _ = require('lodash')
+const { getBaseurl, getenv, ncp } = require('./utils')
+const fg = require('fast-glob')
+const fsPromises = require('fs').promises
+const htmlMinifier = require('html-minifier').minify
+const log = require('debug')('app:index')
+const path = require('path')
+const pug = require('pug')
+const UglifyJS = require('uglify-es')
+
+exports.build = async () => {
+  const PUG_OPTIONS = {
+    basedir: path.resolve(__dirname),
+    baseurl: getBaseurl(),
+    NODE_ENV: getenv('NODE_ENV', 'production'),
+  }
+
+  const htmlMinifierOptions = {
+    caseSensitive: true,
+    collapseBooleanAttributes: true,
+    collapseInlineTagWhitespace: true,
+    collapseWhitespace: true,
+    conservativeCollapse: true,
+    decodeEntities: true,
+    minifyCSS: true,
+    minifyJS: code => UglifyJS.minify(code).code,
+    removeCDATASectionsFromCDATA: true,
+    removeComments: true,
+    removeCommentsFromCDATA: true,
+    removeEmptyAttributes: true,
+    removeRedundantAttributes: true,
+    removeScriptTypeAttributes: true,
+    removeStyleLinkTypeAttributes: true,
+    sortAttributes: true,
+    sortClassName: true,
+    useShortDoctype: true,
+  }
+
+  await ncp('src', 'dist', {
+    stopOnErr: true,
+    filter: filename => !/\.pug$/.test(filename), // 複製除了 .pug 之外的檔案
+  })
+
+  // compile pug files
+  const pugFiles = _.map(await fg('src/**/*.pug'), file => file.slice(4))
+
+  for (const file of pugFiles) {
+    try {
+      const html = htmlMinifier(pug.renderFile(path.resolve(__dirname, 'src', file), PUG_OPTIONS), htmlMinifierOptions)
+      const dist = path.resolve(__dirname, 'dist', file.replace(/\.pug$/, '.html'))
+      await fsPromises.mkdir(path.dirname(dist), { recursive: true })
+      await fsPromises.writeFile(dist, html)
+    } catch (err) {
+      log(file, err)
+      throw err
+    }
+  }
+}
---
+++ utils.js
@@ -0,0 +1,16 @@
+const _ = require('lodash')
+const ncp = require('ncp').ncp
+
+exports.getenv = (key, defaultval) => _.get(process, ['env', key], defaultval)
+
+exports.getBaseurl = (() => {
+  const baseurl = _.trimEnd(exports.getenv('BASEURL', 'https://suninx.com.tw/'), '/') + '/'
+  return () => baseurl
+})()
+
+exports.ncp = (() => {
+  ncp.limit = 16
+  return (source, destination, options) => new Promise((resolve, reject) => {
+    ncp(source, destination, options, err => err ? reject(err) : resolve())
+  })
+})()
